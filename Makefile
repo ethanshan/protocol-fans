f83e5a7c60225306cac1838ae0c7cd80b1b1eee4
Ethan Shan <shan0xiao0xi@gmail.com>
2014-05-09 21:55:14 +0800

    [ping] add ping code arch
    1. add ping.c/ping.h
    2. add Makefile
    3. add Ethernet/IP framework structure ascii diagram
    4. add ncurse use example
    
    Signed-off-by: Ethan Shan <shan0xiao0xi@gmail.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,30 @@
+#         _.._   _..---.                                
+#      .-"    ;-"       \                               
+#     /      /           |                              
+#    |      |       _=   |      I SPY WITH MY LITTLE EYE
+#    ;   _.-'\__.-')     |      ................. A DICK
+#     `-'      |   |    ;                               
+#              |  /;   /      _,                        
+#            .-.;.-=-./-""-.-` _`                       
+#           /   |     \     \-` `,                      
+#          |    |      |     |                          
+#          |____|______|     |                          
+#           \0 / \0   /      /                          
+#        .--.-""-.`--'     .'                           
+#       (#   )          ,  \                            
+#       ('--'          /\`  \                           
+#        \       ,,  .'      \                          
+#         `-._    _.'\        \                         
+#             `""`    \        \                        
+#
 CC=gcc
-CFLAGS=-I.
+CFLAGS=-I. -lcurses
 
 pfans: main.o
-	$(CC) -o pfans main.o
+	$(CC) -o pfans main.o $(CFLAGS)
 
 main.o: src/main.c src/config.h
-	$(CC) -c src/main.c -o main.o
+	$(CC) -c src/main.c -o main.o $(CFLAGS)
 
 clean:
 	rm *.o pfans
--- src/main.c
+++ src/main.c
@@ -10,12 +10,72 @@
  *
  */
 
+//           ___                                        
+//         ,'_  "`-.      ,-'""`-.                      
+//        ('"/"-.   \    /  ,-,.  \
+//         `'    \  ,-'-/    /  `-'                     
+//               ,-'-.      /                           
+//            __ ("|")     f   NUGGGGGGGGGGGGGGGGGGGGGGH
+//           (_)`-"---.    |                            
+//            l   ---.     j   I'M A BUNNY       
+//             `---'     ,'                             
+//                 \    f      HUUUUUUUUUUUUUUGGHHGHHHHH
+//                  )   l                               
+//               __f _   Y     I love  carrot
+//            ,'",-'"_"  l                              
+//           (,,(,,,' `   Y                             
+//                |       l                             
+//                |        \,';,                        
+//                l    ,    Y, ;                        
+//                (`._(     ),'                         
+//                 `.  `.  (                            
+//               ,--',--'   )                         
+//              (,,,(,,,---'                            
+
+#include <linux/icmp.h>
+//#include <netinet/ip_icmp.h>
+
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <ncurses.h>
 
 #include "config.h"
 
+#define MAX_WAIT_TIME 100
+
 int main(int argc, char** argv)
 {
+    struct hostent *host;
+    struct protoent *protocol;
+    unsigned long inaddr = 0l;
+    int waittime = MAX_WAIT_TIME;
+    int size = 50 * 1024;
+    if (argc < 2) {
+        printf("Usage:%s hostname/IP address\n", argv[0]);
+        exit(1);
+    }
+
+    // Obtain icmp protocol entry
+    if ((protocol = getprotobyname("icmp")) == NULL) {
+        perror("getprotobyname call error");
+        exit(1);
+    }
+    int sockfd;
+    // Create raw socket
+    if ((sockfd = socket(AF_INET, SOCK_RAW, protocol->p_proto)) < 0) {
+        perror("socket error");
+        exit(1);
+    }
+    setuid(getutid());
+    initscr();
+
+    printw("Hello");
+    refresh();
+    getchar();
+    endwin();
     printf("APP:\t%s\nVERSION:\t%s\n", APP_NAME, VERSION);
     return 0;
 }
